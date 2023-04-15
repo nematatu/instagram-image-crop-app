765c46cafa72cab736087fc439dc2845437dd683
nema <nematatu1003@gmail.com>
2023-04-16 04:05:31 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Myapp());
-}
+void main() => runApp(Myapp());
 
 class Myapp extends StatelessWidget {
   const Myapp({Key? key}) : super(key: key);
-  final title = 'FlutterSample';
-  final message = 'sample message';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Sample',
-      home: MyhomePage(title: this.title, message: this.message),
+      title: 'hello flutter',
+      home: MyHomePage(title: 'flutter test'),
     );
   }
 }
 
-class MyhomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-  final String message;
-  const MyhomePage({
-    Key? key,
-    required this.title,
-    required this.message,
-  }) : super(key: key);
   @override
-  _MyhomePageState createState() => _MyhomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyhomePageState extends State<MyhomePage> {
+class _MyHomePageState extends State<MyHomePage> {
+  static var _message = 'ok.';
+  static var _janken = <String>['グー', 'チョキ', 'パー'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Text(
-        widget.message,
-        style: TextStyle(fontSize: 35),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                _message,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "Roboto"),
+              ),
+            ),
+            TextButton(
+              onPressed: ButtonPressed,
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  'Push me!',
+                  style: TextStyle(
+                    fontSize: 42.0,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Roboto',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void ButtonPressed() {
+    setState(() {
+      _message = (_janken..shuffle()).first;
+    });
   }
 }
